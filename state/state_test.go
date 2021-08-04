657ea84559556ee8d098fa7e3fec98406b259916
Austin Roberts <austin.roberts@openrelay.xyz>
2021-08-04 15:56:07 -0500

    Add tests for the statedb implementation
    Derived from the Geth statedb tests.

---
+++ state/state_test.go
@@ -0,0 +1,192 @@
+package state
+
+import (
+  "math/big"
+  "testing"
+  "github.com/openrelayxyz/cardinal-storage"
+  "github.com/openrelayxyz/cardinal-types"
+  "github.com/openrelayxyz/cardinal-evm/common"
+)
+
+func testWithStateDB(fn func(sdb StateDB) error) error {
+  sdb := NewMemStateDB(1, 128)
+  sdb.storage.AddBlock(
+    types.HexToHash("a"),
+    types.Hash{},
+    1,
+    big.NewInt(1),
+    []storage.KeyValue{
+      storage.KeyValue{Key: []byte("/a/Data"), Value: []byte("Something")},
+      storage.KeyValue{Key: []byte("/a/Data2"), Value: []byte("Something Else")},
+      storage.KeyValue{Key: []byte("a"), Value: []byte("1")},
+      storage.KeyValue{Key: []byte("b"), Value: []byte("2")},
+    },
+    [][]byte{},
+    []byte("0"),
+  )
+  return sdb.View(types.HexToHash("a"), fn)
+}
+
+
+func TestNull(t *testing.T) {
+  if err := testWithStateDB(func(sdb StateDB) error {
+    address := common.HexToAddress("0x823140710bf13990e4500136726d8b55")
+    sdb.CreateAccount(address)
+    //value := common.FromHex("0x823140710bf13990e4500136726d8b55")
+    var value types.Hash
+
+    sdb.SetState(address, types.Hash{}, value)
+    sdb.Finalise()
+
+    if value := sdb.GetState(address, types.Hash{}); value != (types.Hash{}) {
+      t.Errorf("expected empty current value, got %x", value)
+    }
+    if value := sdb.GetCommittedState(address, types.Hash{}); value != (types.Hash{}) {
+      t.Errorf("expected empty committed value, got %x", value)
+    }
+    return nil
+  }); err != nil { t.Errorf(err.Error()) }
+}
+
+
+func TestSnapshot(t *testing.T) {
+  stateobjaddr := common.BytesToAddress([]byte("aa"))
+  var storageaddr types.Hash
+  data1 := types.BytesToHash([]byte{42})
+  data2 := types.BytesToHash([]byte{43})
+  if err := testWithStateDB(func(sdb StateDB) error {
+    // snapshot the genesis state
+    genesis := sdb.Snapshot()
+
+    // set initial state object value
+    sdb.SetState(stateobjaddr, storageaddr, data1)
+    snapshot := sdb.Snapshot()
+
+    // set a new state object value, revert it and ensure correct content
+    sdb.SetState(stateobjaddr, storageaddr, data2)
+    sdb.RevertToSnapshot(snapshot)
+
+    if v := sdb.GetState(stateobjaddr, storageaddr); v != data1 {
+      t.Errorf("wrong storage value %v, want %v", v, data1)
+    }
+    if v := sdb.GetCommittedState(stateobjaddr, storageaddr); v != (types.Hash{}) {
+      t.Errorf("wrong committed storage value %v, want %v", v, types.Hash{})
+    }
+
+    // revert up to the genesis state and ensure correct content
+    sdb.RevertToSnapshot(genesis)
+    if v := sdb.GetState(stateobjaddr, storageaddr); v != (types.Hash{}) {
+      t.Errorf("wrong storage value %v, want %v", v, types.Hash{})
+    }
+    if v := sdb.GetCommittedState(stateobjaddr, storageaddr); v != (types.Hash{}) {
+      t.Errorf("wrong committed storage value %v, want %v", v, types.Hash{})
+    }
+    return nil
+  }); err != nil { t.Errorf(err.Error() )}
+}
+
+func TestCopySnapshot(t *testing.T) {
+  stateobjaddr := common.BytesToAddress([]byte("aa"))
+  var storageaddr types.Hash
+  data1 := types.BytesToHash([]byte{42})
+  data2 := types.BytesToHash([]byte{43})
+  if err := testWithStateDB(func(sdb StateDB) error {
+    // snapshot the genesis state
+    genesis := sdb.Snapshot()
+
+    // set initial state object value
+    sdb.SetState(stateobjaddr, storageaddr, data1)
+    snapshot := sdb.Snapshot()
+
+    // set a new state object value, revert it and ensure correct content
+    sdb.SetState(stateobjaddr, storageaddr, data2)
+    sdb.RevertToSnapshot(snapshot)
+
+    sdb2 := sdb.Copy()
+
+    // revert up to the genesis state and ensure correct content
+    sdb2.RevertToSnapshot(genesis)
+
+    // Verify that sdb2 has been reverted and sdb has not
+
+    if v := sdb.GetState(stateobjaddr, storageaddr); v != data1 {
+      t.Errorf("wrong storage value %v, want %v", v, data1)
+    }
+    if v := sdb.GetCommittedState(stateobjaddr, storageaddr); v != (types.Hash{}) {
+      t.Errorf("wrong committed storage value %v, want %v", v, types.Hash{})
+    }
+
+    if v := sdb2.GetState(stateobjaddr, storageaddr); v != (types.Hash{}) {
+      t.Errorf("wrong storage value %v, want %v", v, types.Hash{})
+    }
+    if v := sdb2.GetCommittedState(stateobjaddr, storageaddr); v != (types.Hash{}) {
+      t.Errorf("wrong committed storage value %v, want %v", v, types.Hash{})
+    }
+    return nil
+  }); err != nil { t.Errorf(err.Error() )}
+}
+
+
+func TestSnapshotEmpty(t *testing.T) {
+  if err := testWithStateDB(func(sdb StateDB) error {
+    sdb.RevertToSnapshot(sdb.Snapshot())
+    return nil
+  }); err != nil  {t.Errorf( err.Error() )}
+}
+
+
+func TestSnapshot2(t *testing.T) {
+
+  if err := testWithStateDB(func(s StateDB) error {
+    sdb := s.(*stateDB)
+    stateobjaddr0 := common.BytesToAddress([]byte("so0"))
+    stateobjaddr1 := common.BytesToAddress([]byte("so1"))
+    var storageaddr types.Hash
+
+    data0 := types.BytesToHash([]byte{17})
+    data1 := types.BytesToHash([]byte{18})
+
+    sdb.SetState(stateobjaddr0, storageaddr, data0)
+    sdb.SetState(stateobjaddr1, storageaddr, data1)
+
+    // db, trie are already non-empty values
+    so0 := sdb.getAccount(stateobjaddr0)
+    so0.addBalance(big.NewInt(42))
+    so0.setNonce(43)
+    so0.setCode([]byte{'c', 'a', 'f', 'e'})
+    so0.suicided = false
+    so0.deleted = false
+
+    sdb = sdb.Copy().(*stateDB)
+
+    // and one with deleted == true
+    so1 := sdb.getAccount(stateobjaddr1)
+    so1.addBalance(big.NewInt(52))
+    so1.setNonce(53)
+    so1.setCode([]byte{'c', 'a', 'f', 'e', '2'})
+    so1.suicided = true
+    so1.deleted = true
+
+    so1 = sdb.getAccount(stateobjaddr1)
+    if !so1.deleted {
+      t.Fatalf("deleted object not nil when getting")
+    }
+
+    snapshot := sdb.Snapshot()
+    sdb.RevertToSnapshot(snapshot)
+
+    so0Restored := sdb.getAccount(stateobjaddr0)
+    // Update lazily-loaded values before comparing.
+    so0Restored.getState(sdb.tx, sdb.chainid, storageaddr)
+    so0Restored.getCode(sdb.tx, sdb.chainid)
+    // non-deleted is equal (restored)
+    if !so0Restored.equal(so0) { t.Errorf("Mismatch") }
+
+    // deleted should be nil, both before and after restore of state copy
+    so1Restored := sdb.getAccount(stateobjaddr1)
+    if !so1Restored.deleted {
+      t.Fatalf("deleted object not nil after restoring snapshot: %+v", so1Restored)
+    }
+    return nil
+  }); err != nil  {t.Errorf( err.Error() )}
+}
