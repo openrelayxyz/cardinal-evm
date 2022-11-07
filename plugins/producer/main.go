662aad8af4e9ce2a02acfc03366d9801011c8277
Austin Roberts <austin.roberts@rivet.cloud>
2022-11-07 13:42:12 -0600

    Align cli import with bor

--- plugins/producer/main.go
+++ plugins/producer/main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/openrelayxyz/plugeth-utils/restricted/hexutil"
 	"github.com/savaki/cloudmetrics"
 	"github.com/pubnub/go-metrics-statsd"
-	"github.com/urfave/cli/v2"
+	"gopkg.in/urfave/cli.v1"
 	"strings"
 	"sync"
 )
