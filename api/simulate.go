1c0a77e54f683165601b5d7efea5ba6163456b79
Jesse <akohgodwinjesse@gmail.com>
2025-08-08 22:07:39 +0100

    remove extra finalise call

--- api/simulate.go
+++ api/simulate.go
@@ -193,7 +193,7 @@ func (s *simulator) processBlock(ctx *rpc.CallContext, block *simBlock, header, 
 		callResults = make([]simCallResult, len(block.Calls))
 		receipts    = make([]*types.Receipt, len(block.Calls))
 		senders     = make(map[ctypes.Hash]common.Address)
-		allLogs     []*types.Log
+		// allLogs     []*types.Log
 	)
 
 	getHashFn := func(n uint64) ctypes.Hash {
@@ -300,10 +300,9 @@ func (s *simulator) processBlock(ctx *rpc.CallContext, block *simBlock, header, 
 			}
 		} else {
 			callRes.Status = hexutil.Uint64(types.ReceiptStatusSuccessful)
-			allLogs = append(allLogs, receipt.Logs...)
+			// allLogs = append(allLogs, receipt.Logs...)
 		}
 		callResults[i] = callRes
-		s.state.Finalise()
 	}
 
 	header.GasUsed = gasUsed
