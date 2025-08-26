c4f861c11df8a519ee20988be59dd9005bf73884
Jesse <akohgodwinjesse@gmail.com>
2025-08-26 21:24:39 +0100

    add BlobBaseCost

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -180,6 +180,8 @@ const (
 	// up to half the consumed gas could be refunded. Redefined as 1/5th in EIP-3529
 	RefundQuotient        uint64 = 2
 	RefundQuotientEIP3529 uint64 = 5
+
+	BlobBaseCost                       = 1 << 13 // Base execution gas cost for a blob.
 )
 
 // Gas discount table for BLS12-381 G1 and G2 multi exponentiation operations
